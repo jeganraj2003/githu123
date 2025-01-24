af26e2134400b6d1bd8308083300d4d6ba15d986
jeganraj2003 <rajjegan133@gmail.com>
2025-01-24 08:00:08 +0530

    python avg

---
+++ avf.py
@@ -0,0 +1,11 @@
+num1= int(input("Enetr a number:"))
+
+num2=int (input("enter a number:"))
+
+num3=int(input("enter a number:"))
+
+sum=num1+sum2+num3
+
+avg=sum/3
+
+print(format(num1,num2,num3,avg))
